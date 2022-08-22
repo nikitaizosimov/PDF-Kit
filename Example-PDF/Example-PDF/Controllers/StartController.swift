f22f7fd7970c6327042f7de649f3cc415b145d86
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2022-08-23 00:25:55 +0300

    create example project

---
+++ Example-PDF/Example-PDF/Controllers/StartController.swift
@@ -0,0 +1,20 @@
+//
+//  StartController.swift
+//  Example-PDF
+//
+//  Created by Nikita Izosimov on 23.08.2022.
+//
+
+import UIKit
+
+class StartController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
