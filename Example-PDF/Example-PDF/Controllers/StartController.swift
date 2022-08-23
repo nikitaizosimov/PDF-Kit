91657b9a90f93319137df36cbd5cd32c590dd9b9
nikitaizosimov <48530918+nikitaizosimov@users.noreply.github.com>
2022-08-23 09:42:03 +0300

    update StartController

--- Example-PDF/Example-PDF/Controllers/StartController.swift
+++ Example-PDF/Example-PDF/Controllers/StartController.swift
@@ -6,15 +6,24 @@
 //
 
 import UIKit
+import PDF_Kit
 
 class StartController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
     }
-
-
+    
+    @objc
+    private func addTapped() {
+        let controller = PDFController()
+        
+        controller.view.backgroundColor = .black
+        
+        navigationController?.pushViewController(controller, animated: true)
+    }
 }
-
